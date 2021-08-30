6db77a7a7e7ccf838883bc982c23fa199f5f6f50
maciek97x <maciek97x@gmail.com>
2021-08-30 19:47:27 +0200

    Driver init

---
+++ movement/config.py
@@ -0,0 +1,12 @@
+pin_endstop_X_min = 27
+pin_endstop_X_max = 22
+pin_endstop_Y_min = 23
+pin_endstop_Y_max = 24
+
+pin_motor_A_step  = 26
+pin_motor_A_dir   = 25
+pin_motor_B_step  = 6
+pin_motor_B_dir   = 5
+
+pin_magnet        = 12
+pin_servo         = 13
