730e5ce5cda947de444dfe2b5b934c374271d115
hubicios <hubicios@protonmail.com>
2021-12-07 11:54:35 +0100

    Remove unused admin url

--- src/web/django_project/urls.py
+++ src/web/django_project/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import include
 from django.urls import path
-# from django.contrib import admin
 
 urlpatterns = [
     path('', include('apps.checkers.urls')),
-    # path('admin/', admin.site.urls),
 ]
